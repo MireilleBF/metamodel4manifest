dfcaccc4d929af21c4d451cc27406a20c619fdd1
Mireille Blay-Fornarino <mireilleBF@gmail.com>
2026-03-25 00:53:37 +0100

    fix again

--- DSL4Pipelines/tests/tools/transformations/test_toJson.py
+++ DSL4Pipelines/tests/tools/transformations/test_toJson.py
@@ -9,8 +9,8 @@ def test_to_json():
     print(f"Base directory: {BASE_DIR}")
     # Ensuite, tu construis ton chemin à partir de la racine du projet
     #read yaml file and transform it to json
-    file = BASE_DIR /"tests/examples/sources/nanoGPT_manifest3.yaml"
-    output_dir = "examples/outputs/"
+    file = str(BASE_DIR /"tests/examples/sources/nanoGPT_manifest3.yaml")
+    output_dir = str(BASE_DIR /"tests/examples/outputs/")
     outputPath = yaml_to_json(file,output_dir)
     print(f"Json file generated in : {outputPath}")
 
